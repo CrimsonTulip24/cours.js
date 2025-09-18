8b63d46e3276d1ff171f7dd26ca8fdded0890f86
CrimsonTulip24 <angegodart99@orange.fr>
2025-09-18 12:00:10 +0200

    Exercice 3

--- src/test_vehicule.js
+++ src/test_vehicule.js
@@ -1,31 +1,18 @@
 import Vehicule from './vehicule.js';
 
-const voiture 
+const voiture = new Vehicule("Mercedes CLK", 4, 250);
+const moto = new Vehicule("Honda CBR", 2, 280);
 
-/* Exercice 3 :
+function boost(){
+    return this.vitesse += 50;
+}
 
+function rapide(a,b){
+    if(a.vitesse > b.vitesse) {
+        return `${a.nom} est plus rapide.`
+    } else {
+        return `${b.nom} est plus rapide.`
+    }
+}
 
--Instancier 2 nouveaux Vehicules dans le fichier test_vehicule.js avec les valeurs suivantes :
--Objet voiture (nomVehicule = « Mercedes CLK », nbrRoue = 4, vitesse 250),
--Objet moto (nomVehicule = « Honda CBR », nbrRoue = 2, vitesse = 280),
-
--Créer dans la classe Vehicule une fonction detect() qui :
-détecte si le véhicule est une moto ou une voiture (la méthode retourne une string moto ou voiture avec return).
-
--Exécuter la méthode detect() sur les 2 objets voiture et moto dans le fichier test_vehicule.js.
--Afficher le type de Vehicule dans le fichier test_vehicule.js,
-
--Créer une méthode boost qui ajoute + 50 à la vitesse d’un objet dans le fichier de classe vehicule.js,
-
--Appliquer la méthode boost a la voiture dans le fichier test_vehicule.js,
--Afficher la nouvelle vitesse de la voiture dans le fichier test_vehicule.js.
-Bonus :
--Créer une méthode plusRapide() dans le fichier vehicule.js (classe Vehicule) qui : 
-Prend un object Vehicule en paramètre et
-compare la vitesse des différents véhicules (this et celui en paramètre) et retourne le Vehicule le plus rapide des 2 avec un return.
-Sous la forme : le vehicule : "nom du vehicule" est le plus rapide
-
--Exécuter la méthode plusRapide() dans le fichier test_vehicule.js.
--Afficher le Vehicule le plus rapide dans le fichier test_vehicule.js.
-
-NB : Tous les affichages se feront dans la console. */
+console.log(rapide(moto, voiture))
--- src/vehicule.js
+++ src/vehicule.js
@@ -4,4 +4,33 @@ export default class Vehicule {
         this.roue = nbrRoue;
         this.vitesse = vitesse;
     }
+    detect() {
+        if(this.roue == 2) {
+            return "Moto";
+        } else if(this.roue == 4) {
+            return "Voiture"; 
+        } else {
+            return "Indiquez le nombre de roues."
+        }
+    }
 }
+
+/* Exercice 3 :
+
+-Exécuter la méthode detect() sur les 2 objets voiture et moto dans le fichier test_vehicule.js.
+-Afficher le type de Vehicule dans le fichier test_vehicule.js,
+
+-Créer une méthode boost qui ajoute + 50 à la vitesse d’un objet dans le fichier de classe vehicule.js,
+
+-Appliquer la méthode boost a la voiture dans le fichier test_vehicule.js,
+-Afficher la nouvelle vitesse de la voiture dans le fichier test_vehicule.js.
+Bonus :
+-Créer une méthode plusRapide() dans le fichier vehicule.js (classe Vehicule) qui : 
+Prend un object Vehicule en paramètre et
+compare la vitesse des différents véhicules (this et celui en paramètre) et retourne le Vehicule le plus rapide des 2 avec un return.
+Sous la forme : le vehicule : "nom du vehicule" est le plus rapide
+
+-Exécuter la méthode plusRapide() dans le fichier test_vehicule.js.
+-Afficher le Vehicule le plus rapide dans le fichier test_vehicule.js.
+
+NB : Tous les affichages se feront dans la console. */
