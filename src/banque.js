5c89bb000c16c5916c40244331b791f69e6866b7
CrimsonTulip <angegodart99@orange.fr>
2025-09-23 12:21:25 +0200

    Exercice terminé

--- src/banque.js
+++ src/banque.js
@@ -1,7 +1,20 @@
-import CompteBancaire from "compteBanque.js";
+import CompteBancaire from "./compteBanque.js";
 
-const Alex = new CompteBancaire(Alex, 1000);
-const Clovis = new CompteBancaire(Clovis, 1000);
-const Marco = new CompteBancaire(Marco, 1000);
+const Alex = new CompteBancaire("Alex", 0);
+const Clovis = new CompteBancaire("Clovis", 0);
+const Marco = new CompteBancaire("Marco", 0);
 
-console.log(Alex);
+const client = [Alex, Clovis, Marco]
+
+for(let i = 0; i<client.length; i++){
+    client[i].ajout(1000);
+}
+Marco.virement(Clovis,300);
+try{
+Alex.retrait(1200);
+} catch(Error) {
+    console.log(Error.message)
+}
+for(let i = 0; i<client.length; i++){
+console.log(client[i].nom +", solde : " + client[i].solde);
+}
--- src/compteBanque.js
+++ src/compteBanque.js
@@ -3,4 +3,26 @@ export default class CompteBancaire{
         this.nom = nom;
         this.solde = solde;
     }
+
+    retrait(montant){
+        if(montant > 0 && montant <= this.solde){
+            console.log(`Retrait de ${montant} pour ${this.nom}`);
+            return this.solde -= montant;
+        } else {
+            throw new Error("Vous ne pouvez pas effectuer ce retrait")
+        }
+    }
+    ajout(montant){
+        console.log(`Ajout de ${montant} pour ${this.nom}`);
+        return this.solde += montant;
+    }
+    virement(a,montant){
+        if(montant > 0 && montant <= this.solde){
+            console.log(`Virement de ${montant} de ${this.nom} vers ${a.nom}`);
+            this.retrait(montant);
+            a.ajout(montant);        
+        } else {
+            throw new Error("Vous ne pouvez pas effectuer ce virement")
+        }
+    }
 }
